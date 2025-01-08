725abc15f647320cdf165d5aa63ca7e18580776a
fanny <fanny.zotter@gmail.com>
2025-01-08 20:58:10 +0100

    wip

--- bicycleModelParam.m
+++ bicycleModelParam.m
@@ -1,7 +1,8 @@
-my_data = resreader(['C:\Users\fanny\Desktop\Schwimmwinkelschaetzer\Simulation\BEAR24_v20240206_step.res']);
+my_data = resreader(['C:\Users\fanny\Desktop\Schwimmwinkelschaetzer\Simulation\BEAR24_v20240206_crc.res']);
 time = my_data.System_Time;
 
-speed = my_data.System_Mass_Center_Speed;
+speed = [time, my_data.System_Mass_Center_Speed];
+acceleration = [time, my_data.Vehicle_States_longitudinal_acc_wrt_road];
 sensor_speed_x = [time, my_data.Sensor_Vx];
 sensor_speed_y = [time, my_data.Sensor_Vy];
 sensor_yawrate = [time, my_data.Vehicle_States_yaw_angle];
@@ -16,6 +17,7 @@ diff_front_left= [time, my_data.differential_front_wheel_front_left_alpha];
 diff = [time, my_data.differential_front_wheel_front_right_angle];
 xpo = [time, my_data.Vehicle_CM_Global_X];
 ypo = [time, my_data.Vehicle_CM_Global_Y];
+torque = [time, my_data.motor_to_rear_right_wheel_torque];
 % bicycle model parameters provided by GreanBEAR member Nils Haage
 
 m_veh = 215.000; %[kg] vehicle mass
@@ -24,7 +26,7 @@ m = m_veh + m_drv; % [kg] overall mass
 L = 1.570; %[m] wheel base
 lv =0.874; %[m] distance from center of mass to front axle
 lh = L-lv; %[m] distance from center of mass to rear axle
-Cy_f = 13223; %[N/rad] front tire corner stiffness
+Cy_f = 13223; %[N/rad] front tire corner stirearwheelffness
 Cy_r = 13223; %[N/rad] rear tire corner stiffness
 Thetaz = 124.403600; %[kg*m^2] yaw polar inertia
 Fz = 2150;
